a0d914f9bb9ed97308672efe96bd015d35efa037
Ferkku <late.pudas@gmail.com>
2024-10-28 18:45:00 +0200

    user stories

--- bowling.py
+++ bowling.py
@@ -6,18 +6,45 @@ class BowlingGame:
 
     def __init__(self):
         self.frames = []
-    
+        self.first_bonus = 0
+        self.second_bonus = 0
+
     def add_frame(self, frame: Frame) -> None:
+        if len(self.frames) == 10:
+            raise BowlingError
         self.frames.append(frame)
 
     def get_frame_at(self, i: int) -> Frame:
+        if i >= len(self.frames):
+            raise BowlingError
         return self.frames[i]
 
     def calculate_score(self) -> int:
-        pass
+        score_sum = 0
+        is_perfect = True
+        for i in range(len(self.frames)):
+            frame = self.frames[i]
+            if frame.score() < 10 or frame.is_spare():
+                is_perfect = False
+            frame_score = frame.score()
+            if i < len(self.frames) -1:
+                if frame.is_spare():
+                    frame_score += self.frames[i+1].get_first_throw()
+                if frame.is_strike():
+                    frame_score += self.frames[i + 1].score()
+                    if self.frames[i+1].is_strike() and i < len(self.frames) - 2:
+                        frame_score += self.frames[i + 2].get_first_throw()
+
+            score_sum += frame_score
+
+        if is_perfect:
+            return 300
+        return score_sum + self.first_bonus + self.second_bonus
 
     def set_first_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        if self.frames[-1].is_spare() or self.frames[-1].is_strike():
+            self.first_bonus = bonus_throw
 
     def set_second_bonus_throw(self, bonus_throw: int) -> None:
-        pass
+        if self.frames[-1].is_strike():
+            self.second_bonus = bonus_throw
--- test/test_bowling.py
+++ test/test_bowling.py
@@ -12,3 +12,180 @@ class TestBowlingGame(unittest.TestCase):
         game = BowlingGame()
         game.add_frame(f)
         self.assertEqual(f, game.get_frame_at(0))
+
+    def test_game_created_11_frames(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+        f = Frame(1, 1)
+
+        self.assertRaises(BowlingError, game.add_frame, f)
+
+    def test_game_calculate_game_score(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(81, score)
+
+    def test_game_score_with_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 9))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(88, score)
+
+    def test_game_score_with_strike(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(94, score)
+
+    def test_game_score_with_strike_and_spare(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(4, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(103, score)
+
+    def test_game_score_with_multiple_strikes(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(10, 0))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(112, score)
+
+    def test_game_score_with_multiple_spares(self):
+        game = BowlingGame()
+        game.add_frame(Frame(8, 2))
+        game.add_frame(Frame(5, 5))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 6))
+
+        score = game.calculate_score()
+
+        self.assertEqual(98, score)
+
+    def test_game_score_spare_last_frame(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(2, 8))
+
+        game.set_first_bonus_throw(7)
+
+        score = game.calculate_score()
+
+        self.assertEqual(90, score)
+
+    def test_game_score_strike_last_frame(self):
+        game = BowlingGame()
+        game.add_frame(Frame(1, 5))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(7, 2))
+        game.add_frame(Frame(3, 6))
+        game.add_frame(Frame(4, 4))
+        game.add_frame(Frame(5, 3))
+        game.add_frame(Frame(3, 3))
+        game.add_frame(Frame(4, 5))
+        game.add_frame(Frame(8, 1))
+        game.add_frame(Frame(10, 0))
+
+        game.set_first_bonus_throw(7)
+        game.set_second_bonus_throw(2)
+
+        score = game.calculate_score()
+
+        self.assertEqual(92, score)
+
+    def test_perfect_game(self):
+        game = BowlingGame()
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+        game.add_frame(Frame(10, 10))
+
+        game.set_first_bonus_throw(10)
+        game.set_second_bonus_throw(10)
+
+        score = game.calculate_score()
+
+        self.assertEqual(300, score)
